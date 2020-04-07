c74534108e9c76f55183e4560d4f9c5ba02341ea
Vishal Rana <vishal.ch.36@gmail.com>
2020-04-07 18:48:07 +0530

    Firebase Notification Integrated

---
+++ android/app/src/main/kotlin/com/vishal/noties/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vishal.noties
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+}
